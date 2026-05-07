741f65383ea75395c869156540dc8dd3ba75d970
hornum <a.chere.pnin@mail.ru>
2026-05-07 19:23:21 +0300

    fix user methods add get_or_create_user method

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -4,16 +4,15 @@ from app.db.models import User
 from app.db.session import async_session_maker
 
 
-async def create_user(tg_id: int, chat_id: int, username: str):
+async def create_user(tg_id: int, chat_id: int, username: str) -> User:
     async with async_session_maker() as session:
         user = User(tg_id=tg_id, chat_id=chat_id, username=username)
         session.add(user)
         await session.commit()
-        await session.flush()
-
+        await session.refresh(user)
         return user
 
-async def get_user(tg_id: int, chat_id: int, username: str) -> User:
+async def get_user_or_raise(tg_id: int) -> User:
     async with async_session_maker() as session:
         query = select(User).where(User.tg_id == tg_id)
         result = await session.execute(query)
@@ -22,4 +21,12 @@ async def get_user(tg_id: int, chat_id: int, username: str) -> User:
         if user:
             return user
 
-        return await create_user(tg_id=tg_id, chat_id=chat_id, username=username)
+        raise ValueError("Пользователь не найден. Пожалуйста, введите команду /start")
+
+async def get_or_create_user(tg_id: int, chat_id: int, username: str) -> User:
+    try:
+        user = await get_user_or_raise(tg_id=tg_id)
+    except ValueError:
+        user = await create_user(tg_id=tg_id, chat_id=chat_id, username=username)
+
+    return user
