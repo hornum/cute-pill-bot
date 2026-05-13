057ad6615876790b5b2e3cfb284f2a03a5d3562b
hornum <a.chere.pnin@mail.ru>
2026-05-13 20:00:35 +0300

    fix time parser bug

--- app/service/reminder_service.py
+++ app/service/reminder_service.py
@@ -3,6 +3,6 @@ from datetime import time, datetime
 
 def parse_reminder_time(time_to_remind: str) -> time:
     try:
-        return datetime.strptime(time_to_remind, "%H:%M").time()
+        return datetime.strptime(time_to_remind, "%HH:%MM").time()
     except ValueError:
         raise ValueError("Время должно быть указано в формате ЧЧ:ММ, например 09:30")
